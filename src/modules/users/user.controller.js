aecef8c54111b207a17c6a23e99eb942c8b27959
DamarisM87 <cosmicangel08@gmail.com>
2026-02-20 00:20:32 +0200

    fix: correct import alias for user service in user controller

--- src/modules/users/user.controller.js
+++ src/modules/users/user.controller.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
-import * as UC from "./user.service.js";
+import * as US from "./user.service.js";
 import { authentication } from "../../middleware/authentication.js";
 import { validation } from "../../middleware/validation.js";
 import * as UV from "./user.validation.js"
 
 const userRouter = Router();
-userRouter.post("/signup", validation(UV.signUpSchema), UC.signUp);
-userRouter.post("/signin", validation(UV.signInSchema), UC.signIn);
-userRouter.get("/confirmEmail/:token", UC.confirmEmail);
-userRouter.get("/profile", authentication, UC.getProfile);
-userRouter.post("/signout", authentication, UC.signOut);
-
+userRouter.post("/signup", validation(UV.signUpSchema), US.signUp);
+userRouter.post("/signin", validation(UV.signInSchema), US.signIn);
+userRouter.get("/confirmEmail/:token", US.confirmEmail);
+userRouter.get("/profile", authentication, US.getProfile);
+userRouter.post("/signout", authentication, US.signOut);
+userRouter.post("/refreshToken", US.refreshToken);
 
 export default userRouter;
