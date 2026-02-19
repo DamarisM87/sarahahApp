334b4e29e290989ce3f598c94c54e39c295b54e1
DamarisM87 <cosmicangel08@gmail.com>
2026-02-20 00:20:12 +0200

    fix: update import statements and add message creation route with validation

--- src/modules/messages/message.controller.js
+++ src/modules/messages/message.controller.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import * as MC from "./message.service.js";
+import * as MS from "./message.service.js";
+import { validation } from "../../middleware/validation.js";
+import * as MV from "./message.validation.js"
 
 
 const messageRouter = Router();
 
+messageRouter.post("/",validation(MV.createMessageSchema), MS.createMessage);
 
 
 export default messageRouter;
